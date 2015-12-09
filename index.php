02854f5bd31b4b445ef49ed3c65fb5b8801dfc03
Hui-x-chan <duchaine.kouassi@hotmail.fr>
2015-12-09 09:05:10 +0100

    MOD !!! INDEX.php / STYLE.css !!!

--- index.php
+++ index.php
@@ -1,31 +1,41 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>ProjetTeam</title>
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+           <?php
 
-<body>
+           require_once('bdd/connexionBDD.php');
 
 
 
+           if (isset($_GET["action"])) {
+               $action = $_GET["action"];
+           } else {
+               $action = "main";
+           }
 
+           switch ($action) {
+               case 'aide':
+                   $main = "pages/aide.php";
+                   break;
+               case 'post_formulaire':
+                   $main = "bdd/post_formulaire.php";
+                   break;
+               case 'main':
+                   $main = "pages/main.php";
+                   break;
+               default:
+                   $main = "pages/aide.php";
+                   break;
+           }
 
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</body>
-</html>
-=======
-<!DOCTYPE html>
+           ?>
+
+           <!DOCTYPE html>
 <html lang="Fr">
 
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="biblio_perso/css/bootstrap-3.3.5/css/bootstrap.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/style.css">
 
 
         <title>Projet</title>
@@ -33,154 +43,73 @@
     </head>
 
     <body>
-           
-            <header class="col-sm-12"> <!-- HEADER -->
-              
-               <div class="logo-header"> <!-- LOGO DU SITE -->
-                   <a href="/"><h1>P & P<img class="logo-site" src="img/sale_tag_256.png"></h1></a>
-                </div>
-                <a><p>Site de vente de Particulier à Particulier</p></a>
-                 
-                <nav class="nav cent-nav"> <!-- MENU NAV -->
 
-                    <ul class="nav nav-pills">
+        <header class="col-sm-12"> <!-- HEADER -->
 
-                        <li><a>Accueil</a></li>
-                        <li><a>Depot d'annonce</a></li>
-                        <li><a>Inscription</a></li>
+            <div class="logo-header"> <!-- LOGO DU SITE -->
+                <a href="/"><h1>P & P<img class="logo-site" src="img/sale_tag_256.png"></h1></a>
+            </div>
+            <a><p>Site de vente de Particulier à Particulier</p></a>
 
-                        <li><form method="post"> <!-- LOGIN -->
-                            <li> 
-                                <input type="button" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" value="connexion"> 
-                                <ul class="dropdown-menu">
-                                    <li><p><label>E-mail</label></p></li>
-                                    <li><p><input type="eamil" name="email"></p></li>
-                                    <li><p><label>Mot de passe</label></p></li>
-                                    <li><p><input type="password" id="password" name="password"></p></li>
-                                    <li><p><input type="submit" name="login" value="login"></p></li>    
-                                </ul>
-                            </li>
-                            </form> 
-                        </li><!-- END LOGIN -->
-                    </ul>
-                </nav> <!-- END MENU NAV -->
-                
-            </header> <!-- END HEADER -->
-
-            <nav class="nav cent-nav"> <!-- MENU NAV RECHERCHE -->
+            <nav class="nav cent-nav"> <!-- MENU NAV -->
 
                 <ul class="nav nav-pills">
 
-                    <li><a href="/">Date</a></li>
-                    <li><a href="/">Prix</a></li>
-                    <li><a href="/">Departement</a></li>
-
-                    <li><form method="post">
-                        <li> 
-                            <ul class="nav nav-pills">
-                                <p><li><label>Recherche</label></li>
-                                    <li><input type="search" id="recherche" name="recherche"></li>
-                                    <li><input type="submit" name="recherche" value="ok"></li></p>    
-                            </ul>
-                        </li>
-                        </form> 
-                    </li>
+                    <li><a href="/ProjetTeam/index.php?action=index">Accueil</a></li>
+                    <li><a>Depot d'annonce</a></li>
+                    <li><a href="/ProjetTeam/index.php?action=post_formulaire">Inscription</a></li>
+                    <li><a href="/ProjetTeam/index.php?action=aide">Aide</a></li>
+                    
+                    <?php
+                        include("pages/login.php");
+                    ?>
+                    
                 </ul>
-            </nav> <!-- END MENU NAV RECHERCHE -->
+            </nav> <!-- END MENU NAV -->
 
-            <aside class="col-sm-2"> <!-- TOP ANNONCE -->
-                <fieldset>
-                    <legend>Top Annonce</legend>
-                </fieldset>
+        </header> <!-- END HEADER -->
 
-            </aside> <!-- END TOP ANNONCE -->
+        <nav class="nav cent-nav"> <!-- MENU NAV RECHERCHE -->
 
-            <main class="col-sm-9">
-                <div> <!-- MAIN CONT -->
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
+            <ul class="nav nav-pills">
 
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
+                <li><a href="/">Date</a></li>
+                <li><a href="/">Prix</a></li>
+                <li><a href="/">Departement</a></li>
 
-                    <section>
+                <li><form method="post">
+                    <li> 
                         <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
+                            <p><li><label>Recherche</label></li>
+                                <li><input type="search" id="recherche" name="recherche"></li>
+                                <li><input type="submit" name="recherche" value="ok"></li></p>    
+                        </ul>
+                    </li>
+                    </form> 
+                </li>
+            </ul>
+        </nav> <!-- END MENU NAV RECHERCHE -->
 
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
-                    
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
+        <aside class="col-sm-2"> <!-- TOP ANNONCE -->
+            <fieldset>
+                <legend>Top Annonce</legend>
+            </fieldset>
 
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
+        </aside> <!-- END TOP ANNONCE -->
 
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
+        <main class="col-sm-9">
 
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
-                    
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
+            <?php
+            include($main);
+            ?>
 
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
-
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
-
-                    <section>
-                        <ul class="nav nav-pills">
-                        </ul>   
-                        <p>Texte descript</p>
-                    </section>
-                </div> <!-- END MAIN CONT -->
-            </main>
- <footer class="col-sm-12"> <!-- FOOTER MENU -->
+        </main>
+        <footer class="col-sm-12"> <!-- FOOTER MENU -->
             <section>
                 <h1>Quentin</h1>
             </section>
         </footer> <!-- END FOOTER MENU -->
-  
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
@@ -190,4 +119,3 @@
     </body>
 
 </html>
-s
