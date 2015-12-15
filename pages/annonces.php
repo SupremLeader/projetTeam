7e20fc46f9489d3039454949efa587a842f01e6d
Hui-x-chan <duchaine.kouassi@hotmail.fr>
2015-12-15 16:06:11 +0100

    MOD-------> /* MOD rubaban.css * * annonces.php * /

--- pages/annonces.php
+++ pages/annonces.php
@@ -5,7 +5,7 @@ foreach ($db->query("SELECT * FROM annonces ORDER BY id DESC LIMIT 10") as $anno
     echo <<<EOT
  <div class="container">
   <div class="row">
-  <button  class="btn btn-primary ribbon-b int3">$annonce[titre]     $annonce[date_post]</button>
+  <button  class="btn btn-red ribbon-b int3">$annonce[titre]     $annonce[date_post]</button>
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> 
     
 	<div class="miniature int" style="background-image:url(uploads/$annonce[photo1])">
